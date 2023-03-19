dbcd2df7e330d23b318ca30e1d6daa3401eaca85
mahmed74 <ahmedsheikh0704@gmail.com>
2023-03-19 17:56:50 +0500

    cleanup

--- app.js
+++ app.js
@@ -10,22 +10,6 @@ app.use(cors({ origin: "*" }));
 // A unique identifier for the given session
 const sessionId = uuid.v4();
 
-// app.use(function (req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
-
 app.use(
   bodyParser.urlencoded({
     extended: false,
